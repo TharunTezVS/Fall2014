4f019f6b7481ad8f2008927267c202e4a92c5641
TharunTez <tharuntez4u@gmail.com>
2014-12-06 16:14:01 -0500

    cmt

--- Final/food.php
+++ Final/food.php
@@ -45,14 +45,8 @@
           <li><a href="index.php">Home</a></li>        
           <li><a href="food.php">Food</a></li>
           <li><a href="exercise.php">Exercise</a></li>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+         
+            
           </ul>
         </li>
       </ul>
@@ -63,9 +57,14 @@
         <button type="submit" class="btn btn-default"><i  class="glyphicon glyphicon-search"></i></button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">SignUp</a></li>
+      	<li class = "dropdown">
+      		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <span class="caret"></span></a>
+      		<ul class="dropdown-menu" role="menu">
+      		<li><a href="#">Login</a></li>
+      		<li class="divider"></li>
+            <li><a href="#">Login through a Different account</a></li>
+            
+        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container -->
--- Final/index.php
+++ Final/index.php
@@ -43,15 +43,7 @@
           <li><a href="index.php">Home</a></li>        
           <li><a href="food.php">Food</a></li>
           <li><a href="exercise.php">Exercise</a></li>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
+          
         </li>
       </ul>
       <form class="navbar-form navbar-left" role="search">
@@ -61,9 +53,13 @@
         <button type="submit" class="btn btn-default"><i  class="glyphicon glyphicon-search"></i></button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">SignUp</a></li>
+      	
+        <li class = "dropdown">
+      		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <span class="caret"></span></a>
+      		<ul class="dropdown-menu" role="menu">
+      		<li><a href="#">Login</a></li>
+      		<li class="divider"></li>
+            <li><a href="#">Login through a Different account</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container -->
