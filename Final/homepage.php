fd6d818331fe91a0e9eb4c24dcefcf1749488063
TharunTez <tharuntez4u@gmail.com>
2014-12-06 21:53:37 -0500

    cmt

--- Final/homepage.php
+++ Final/homepage.php
@@ -11,30 +11,11 @@
     <meta name="mobile-web-app-capable" content="yes">
     <link rel="apple-touch-icon" href="css/ico.png">
     <link href="./css/cover.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
+    
 </head>
 
-<body>
-    <div class="modal fade" id="signUp" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-            <h4 class="modal-title" id="myModalLabel">Sign Up</h4>
-          </div>
-          <div class="modal-body">
-            Users are going to be able to sign up here eventually...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
+<body background="fitness.png" bgcolor="#0000FF" bgproperties="fixed">
+    
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
             <div class="cover-container">
@@ -48,7 +29,7 @@
                 </div>
                 <div class="inner cover">
                     <h1 class="cover-heading">Track your Health!</h1>
-                    <p class="lead">Join in and track your exercise and eating habits.</p>
+                    <p class="lead">Fitness is Happiness!</p>
                     <p class="lead">
                         <button class="btn btn-default btn-lg" data-toggle="modal" data-target="#signUp">
                           Sign Up Now
