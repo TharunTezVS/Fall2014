a4378c0ba51785836ad55195a2a6a09f3c614e75
TharunTez <tharuntez4u@gmail.com>
2014-12-06 22:04:36 -0500

    cmt

--- Final/homepage.php
+++ Final/homepage.php
@@ -14,7 +14,7 @@
     
 </head>
 
-<body imgsrc="fitness.png" bgcolor="#0000FF" bgproperties="fixed">
+<body imgsrc="fitness.png" bgcolor="#5cb85c" bgproperties="fixed">
     
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
