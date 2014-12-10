094f9dd0ae809c1fe1bad368dd0d0ad5014c64ac
TharunTez <tharuntez4u@gmail.com>
2014-12-10 16:45:54 -0500

    cmt

--- Final/signin.js
+++ Final/signin.js
@@ -4,19 +4,19 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "raviprov1" && password == "s081691"){
-alert ("Login successfully");
-window.location = "cs.newpaltz.edu/~raviprov1/Fall2014/Final/home.php"; 
-return false;
+ alert ("Login successfully");
+ window.location = "cs.newpaltz.edu/~raviprov1/Fall2014/Final/home.php"; 
+ return false;
 }
 else{
-attempt --;
-alert("You have left "+attempt+" attempt;");
+ attempt --;
+ alert("You have left "+attempt+" attempt;");
 
 if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
+ document.getElementById("username").disabled = true;
+ document.getElementById("password").disabled = true;
+ document.getElementById("submit").disabled = true;
+ return false;
 }
 }
 }
