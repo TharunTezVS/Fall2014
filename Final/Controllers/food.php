fbe1a4d8eb38542e6f81049f680c6f27de243be2
TharunTez <tharuntez4u@gmail.com>
2014-12-11 08:11:47 -0500

    cmt

--- Final/Controllers/food.php
+++ Final/Controllers/food.php
@@ -1,5 +1,5 @@
 <?
-	include_once __DIR__ . '/../inc/_all.php';
+	include_once __DIR__ . '/../inc/all.php';
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : null;
 $method = $_SERVER['REQUEST_METHOD'];
 $format = isset($_REQUEST['format']) ? $_REQUEST['format'] : 'web';
