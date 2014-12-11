6dc5d252bed9ded754698a15d91bf4de4dba5fdd
TharunTez <tharuntez4u@gmail.com>
2014-12-10 23:14:36 -0500

    cmt

--- Final/exercise.php
+++ Final/exercise.php
@@ -15,48 +15,7 @@
 		<div id="top-nav"></div>
 			<header>
 				<div class="container">
-					<nav class="navbar navbar-default" role="navigation">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        
-        
-
-          <li><a href="home.php">Home</a></li>        
-          <li><a href="food.php">Food</a></li>
-          <li><a href="exercise.php">Exercise</a></li>
-         
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default"><i  class="glyphicon glyphicon-search"></i></button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li class = "dropdown">
-      		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <span class="caret"></span></a>
-      		<ul class="dropdown-menu" role="menu">
-      		<li><a href="login.html">Login</a></li>
-      		<li class="divider"></li>
-            <li><a href="#">Login through a Different account</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container -->
-</nav>
+					
 			</header>
 					<h1><center>Track by Your Workout</center></h1>
 				</div>
