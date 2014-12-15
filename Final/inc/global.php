8ca39850a0d3ea394f09e507322f25fd045bea97
TharunTez <tharuntez4u@gmail.com>
2014-12-14 21:00:28 -0500

    cmt

--- Final/inc/global.php
+++ Final/inc/global.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 date_default_timezone_set("America/New_York");
 function GetConnection(){
-	include __DIR__ . '/_password.php';
+	
 	return new mysqli('localhost','raviprov1',$sql_password,'raviprov1_db');
 }
 function my_print($x){
